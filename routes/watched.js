40bd0d574042b50381314b07b4a7609a4a430934
Michel Balamou <michelbalamou@gmail.com>
2019-11-17 21:27:31 -0500

    Added mock watched api

--- routes/watched.js
+++ routes/watched.js
@@ -2,8 +2,18 @@ const router = require("express").Router();
 
 router.get("/watched/:userid", (req, res, next) => {
     const userid = req.params.userid;
+    const TYPE_MOVIE = "movie";
+    const TYPE_SHOW = "show";
 
-    res.send(`<b>${userid}</b>`);
+    const posters = ["https://cdn.flickeringmyth.com/wp-content/uploads/2019/02/vice.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMjIxMjgxNTk0MF5BMl5BanBnXkFtZTgwNjIyOTg2MDE@._V1_.jpg"];
+        
+    const watched = [
+        { id: 3, posterURL: posters[0], stoppedAt: 0.8, label: "1h 3min", movieURL: "/en/shows/E03.mkv", type: TYPE_MOVIE},
+        { id: 2, posterURL: posters[1], stoppedAt: 0.5, label: "S3:E4", movieURL: "/en/shows/E03.mkv", type: TYPE_SHOW}
+    ];
+
+    res.json(watched);
 });
 
 module.exports = router;
