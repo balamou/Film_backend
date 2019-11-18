059b0d59122c3883832eff54b078333696c17796
Michel Balamou <michelbalamou@gmail.com>
2019-11-17 20:47:55 -0500

    Added mock login and signup

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 router.get("/login/:username", (req, res, next) => {
     const username = req.params.username;
 
-    res.send(`<b>${username}</b>`);
+    res.json({ userId: 1 });
 });
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 router.get("/signup/:username", (req, res, next) => {
     const username = req.params.username;
 
-    res.send(`<b>${username}</b>`);
+    res.json({ userId: 2 });
 });
 
 module.exports = router;
