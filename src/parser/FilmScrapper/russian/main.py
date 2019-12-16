d453beae57944ea477612dbe7892dd52fb700574
Michel Balamou <michelbalamou@gmail.com>
2019-12-16 15:04:45 -0500

    Added more kinopoisk retrieval

--- src/parser/FilmScrapper/russian/main.py
+++ src/parser/FilmScrapper/russian/main.py
@@ -1,5 +1,19 @@
-#!/usr/bin/python3
 from kinopoisk.movie import Movie
+import time
 
-movie_list = Movie.objects.search('breaking bad')
-print(movie_list[0].title)
+def main():
+    movie_list = Movie.objects.search('Rick and morty')
+    print(movie_list[0].title)
+    first_movie = movie_list[0]
+
+    movie = Movie(id=first_movie.id)
+    time.sleep(.300)
+    movie.get_content('main_page')
+    print(movie.plot)
+
+    time.sleep(.300)
+    movie.get_content('posters')
+    print(movie.posters)
+
+if __name__ == '__main__':
+    main()
