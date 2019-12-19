ff51d1253ad9ff4b010d124bd5f34bf238d3e4c8
Michel Balamou <michelbalamou@gmail.com>
2019-12-18 21:28:27 -0500

    Added series and episodes table definitions

--- src/database.sql
+++ src/database.sql
@@ -4,8 +4,47 @@ DROP TABLE IF EXISTS USERS;
 DROP TABLE IF EXISTS MOVIES;
 DROP TABLE IF EXISTS EPISODES;
 DROP TABLE IF EXISTS SERIES;
+DROP TYPE IF EXISTS language;
 
-CREATE TABLE IF NOT EXISTS USERS (
-  ID SERIAL,
+-- -----------------------------------------------------------------------------
+-- USERS
+-- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS USERS(
+  ID SERIAL PRIMARY KEY,
   NAME VARCHAR(20)
 );
+
+-- -----------------------------------------------------------------------------
+-- TV SHOW
+-- -----------------------------------------------------------------------------
+CREATE TYPE language AS ENUM ('en', 'ru');
+
+CREATE TABLE SERIES(
+  ID SERIAL PRIMARY KEY,
+  LANGUAGE language NOT NULL DEFAULT 'en',
+  FOLDER VARCHAR(30) NOT NULL,
+  TITLE VARCHAR(50) NOT NULL,
+  SEASONS INT NOT NULL,
+
+  DESCRIPTION VARCHAR(250),
+  POSTER VARCHAR(200),
+
+  DISPLAY BOOLEAN DEFAULT TRUE
+);
+
+
+CREATE TABLE IF NOT EXISTS EPISODES(
+    ID SERIAL PRIMARY KEY, 
+    SERIES_ID INT NOT NULL,
+    EPISODE_NUMBER INT NOT NULL,
+    SEASON_NUMBER INT NOT NULL,
+    VIDEO_URL VARCHAR(200) NOT NULL,
+    DURATION INT NOT NULL,
+
+    THUMBNAIL_URL VARCHAR(200),
+    TITLE VARCHAR(50),
+    PLOT VARCHAR(250),
+
+    CONSTRAINT unique_episode_in_season UNIQUE (ID, SEASON_NUMBER, EPISODE_NUMBER),
+    FOREIGN KEY (SERIES_ID) REFERENCES SERIES(ID)
+)
