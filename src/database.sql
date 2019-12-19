aa2495c3a7e5a3700454564dc0c85d2787e0addf
Michel Balamou <michelbalamou@gmail.com>
2019-12-18 21:41:47 -0500

    Added schema for viewed_episodes

--- src/database.sql
+++ src/database.sql
@@ -1,3 +1,4 @@
+-- PostgreSQL 12.1
 DROP TABLE IF EXISTS VIEWED_EPISODES;
 DROP TABLE IF EXISTS VIEWED_MOVIES;
 DROP TABLE IF EXISTS USERS;
@@ -47,4 +48,20 @@ CREATE TABLE IF NOT EXISTS EPISODES(
 
     CONSTRAINT unique_episode_in_season UNIQUE (ID, SEASON_NUMBER, EPISODE_NUMBER),
     FOREIGN KEY (SERIES_ID) REFERENCES SERIES(ID)
-)
+);
+
+-- -----------------------------------------------------------------------------
+-- VIEWED
+-- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS VIEWED_EPISODES(
+  EPISODE_ID INT NOT NULL,
+  USER_ID INT NOT NULL,
+
+  STOPPED_AT INT NOT NULL, -- between 0 and the duration of the episode
+  DATE_PLAYED TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- first time played
+  DATE_VIEWED TIMESTAMP NOT NULL, -- date last viewed
+
+  CONSTRAINT ID PRIMARY KEY (EPISODE_ID, USER_ID),
+  FOREIGN KEY (EPISODE_ID) REFERENCES EPISODES(ID),
+  FOREIGN KEY (USER_ID) REFERENCES USERS(ID)
+);
