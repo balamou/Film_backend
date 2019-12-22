c9198a4e7df10ae3fac2cf2dac04ad2d2b7e2f8c
Michel Balamou <michelbalamou@gmail.com>
2019-12-21 23:16:15 -0500

    Moved array extensions to a different file

--- src/parser/Tree/DiffTrees.ts
+++ src/parser/Tree/DiffTrees.ts
@@ -1,43 +1,5 @@
 import Tree from "./Tree";
-
-declare global {
-    interface Array<T> {
-        doesInclude<T>(this: T[], rhs: T, predicate: (lhs: T, rhs: T) => boolean): boolean;
-        intersect<T>(this: T[], rhs: T[], predicate: (lhs: T, rhs: T) => boolean): [T, T][];
-    }
-}
-
-function doesInclude<T>(this: T[], rhs: T, predicate: (lhs: T, rhs: T) => boolean): boolean {
-    for (let obj of this) {
-        if (predicate(obj, rhs))
-            return true;
-    }
-
-    return false;
-}
-
-// Returns an array of pairs where each pair contains a left object from lhs array 
-// and a right object from rhs array. Each pari evaluates to true with the equality 
-// predicate `equalityPredicate(left_obj, right_obj)`.
-//
-// Runs in O(n^2) where n is the size of the largest array between `lhs` and `rhs`.
-// Note: the runtime can be improved to O(n*log(n)) by sorting each array first.
-function intersect<T>(this: T[], rhs: T[], equalityPredicate: (lhs: T, rhs: T) => boolean) {
-    const result: [T, T][] = [];
-
-    this.forEach(value => {
-        const matchingObject = rhs.find(x => equalityPredicate(value, x));
-
-        if (!matchingObject) return;
-
-        result.push([value, matchingObject]);
-    });
-
-    return result;
-}
-
-Array.prototype.doesInclude = doesInclude;
-Array.prototype.intersect = intersect;
+import './Array';
 
 function instructions() {
     // d[SERIES] remove from DB
