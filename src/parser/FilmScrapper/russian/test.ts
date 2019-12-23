8ae290e5aa22b5c442757b0deb39b275fc818848
Michel Balamou <michelbalamou@gmail.com>
2019-12-22 21:36:59 -0500

    Fetched more shows into cache

--- src/parser/FilmScrapper/russian/test.ts
+++ src/parser/FilmScrapper/russian/test.ts
@@ -8,6 +8,9 @@ function test() {
         console.log(fetcher.fetchSeries("friends"));
         console.log(fetcher.fetchSeries("game of thrones"));
         console.log(fetcher.fetchSeries("lost"));
+        console.log(fetcher.fetchSeries("better call saul"));
+        console.log(fetcher.fetchSeries("mr robot"));
+        console.log(fetcher.fetchSeries("american crime story"));
     } catch (error) {
         const pythonError = (error as Error).message;
         console.log(pythonError);
