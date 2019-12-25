963db85b947bc3771ccc473b15273b698792bc6a
Michel Balamou <michelbalamou@gmail.com>
2019-12-24 19:17:05 -0500

    Added rootFolder variable for paths in tests

--- tests/flattenFileTree.test.ts
+++ tests/flattenFileTree.test.ts
@@ -5,6 +5,8 @@ import MockDirTreeCreator from './stub/MockDirTreeCreator';
 import MockFSEditor from './stub/MockFSEditor';
 
 describe('reorginize series folders [flatten]', function () {
+    const rootFolder = '/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders';
+
     it('well structured series folder [no changes required]', () => {
         const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
         const flatData = flattenFileTree.findMisplacedFiles('well_orginized_show');
@@ -16,9 +18,9 @@ describe('reorginize series folders [flatten]', function () {
     it('extra non-video files in series top level', () => {
         const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
         const flatData = flattenFileTree.findMisplacedFiles('extra_files_in_top_level');
-        
-        const purge = ['/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders/extra_files_in_top_level/movie_description.txt',
-        '/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders/extra_files_in_top_level/poster.jpeg'];
+
+        const purge = [ `${rootFolder}/extra_files_in_top_level/movie_description.txt`,
+                        `${rootFolder}/extra_files_in_top_level/poster.jpeg` ];
 
         expect(flatData.moveup).to.eql([]);
         expect(flatData.purge).to.eql(purge);
@@ -27,9 +29,9 @@ describe('reorginize series folders [flatten]', function () {
     it('empty folders in series top level', () => {
         const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
         const flatData = flattenFileTree.findMisplacedFiles('empty_folders_in_top_level');
-        
-        const purge =[ "/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders/empty_folders_in_top_level/S3",
-        "/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders/empty_folders_in_top_level/information" ];
+
+        const purge =[ `${rootFolder}/empty_folders_in_top_level/S3`,
+                        `${rootFolder}/empty_folders_in_top_level/information` ];
 
         expect(flatData.moveup).to.eql([]);
         expect(flatData.purge).to.eql(purge);
@@ -39,9 +41,9 @@ describe('reorginize series folders [flatten]', function () {
         const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
         const flatData = flattenFileTree.findMisplacedFiles('folders_with_no_video_files_in_top_level');
         
-        const purge =[ "/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders/folders_with_no_video_files_in_top_level/S3/infromation.txt",
-                        "/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders/folders_with_no_video_files_in_top_level/S3/some_picture.jpeg",
-                        "/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders/folders_with_no_video_files_in_top_level/S3"];
+        const purge =[ `${rootFolder}/folders_with_no_video_files_in_top_level/S3/infromation.txt`,
+                        `${rootFolder}/folders_with_no_video_files_in_top_level/S3/some_picture.jpeg`,
+                        `${rootFolder}/folders_with_no_video_files_in_top_level/S3` ];
 
         expect(flatData.moveup).to.eql([]);
         expect(flatData.purge).to.eql(purge);
