8b24de3413692adb0e147321231ef8eb7374be81
Michel Balamou <michelbalamou@gmail.com>
2019-12-24 21:38:35 -0500

    Added more tests to flatten file tree

--- tests/flattenFileTree.test.ts
+++ tests/flattenFileTree.test.ts
@@ -7,7 +7,7 @@ import MockFSEditor from './stub/MockFSEditor';
 describe('reorginize series folders [flatten]', function () {
     const rootFolder = '/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders';
 
-    it('well structured series folder [no changes required]', () => {
+    it('well structured series folder', () => {
         const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
         const flatData = flattenFileTree.findMisplacedFiles('well_orginized_show');
         
@@ -49,4 +49,43 @@ describe('reorginize series folders [flatten]', function () {
         expect(flatData.purge).to.eql(purge);
     });
 
+    it('folders and non-video files at depth 2', () => {
+        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+        const flatData = flattenFileTree.findMisplacedFiles('folders_and_non_video_files_at_depth_2');
+        
+        const purge = [`${rootFolder}/folders_and_non_video_files_at_depth_2/S1/depth_1_folder`,
+                        `${rootFolder}/folders_and_non_video_files_at_depth_2/S2/depth_1_folder`];
+
+        expect(flatData.moveup).to.eql([]);
+        expect(flatData.purge).to.eql(purge);
+    });
+
+    it('video files at level 3', () => {
+        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+        const flatData = flattenFileTree.findMisplacedFiles('video_files_at_level_3');
+        
+        const moveUp = [`${rootFolder}/video_files_at_level_3/S2/nested_videos/South park [3x02].mkv`,
+                        `${rootFolder}/video_files_at_level_3/S2/nested_videos/South park [3x03].mp4`];
+
+        const purge = [`${rootFolder}/video_files_at_level_3/S2/nested_videos`];
+
+        expect(flatData.moveup).to.eql(moveUp);
+        expect(flatData.purge).to.eql(purge);
+    });
+
+    it('video files below level 2', () => {
+        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+        const flatData = flattenFileTree.findMisplacedFiles('video_files_below_level_2');
+        
+        const moveUp = [`${rootFolder}/video_files_below_level_2/Season 12/nested_videos/South park [3x02].mkv`,
+                        `${rootFolder}/video_files_below_level_2/Season 10/nested_videos/deeply nested/South park [3x02].mkv`,
+                        `${rootFolder}/video_files_below_level_2/Season 10/nested_videos/deeply nested/South park [3x03].mp4`,
+                        `${rootFolder}/video_files_below_level_2/Season 12/nested_videos/deeply nested/South park [3x03].mp4`];
+
+        const purge = [`${rootFolder}/video_files_below_level_2/Season 10/nested_videos`,
+                        `${rootFolder}/video_files_below_level_2/Season 12/nested_videos`];
+
+        expect(flatData.moveup, 'move up wrong').to.eql(moveUp);
+        expect(flatData.purge, 'purge wrong').to.eql(purge);
+    });
 });
