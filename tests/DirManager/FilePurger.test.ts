db1bf635d0ebcbfd1cd9529824bbe6b40be5cc39
Michel Balamou <michelbalamou@gmail.com>
2019-12-25 19:16:47 -0500

    Reorginized test files

--- tests/DirManager/FilePurger.test.ts
+++ tests/DirManager/FilePurger.test.ts
@@ -4,192 +4,204 @@ import MockFSEditor from '../stub/MockFSEditor';
 import YAML from 'yaml';
 import { FSEditor } from '../../src/parser/Adapters/FSEditor';
 
-describe('file purger tests', () => {
+describe('File purger tests', () => {
     const dirToTreeSnapshots = `${__dirname}/expected`;
     const readFile = (path: string) => new FSEditor().readFile(`${dirToTreeSnapshots}/${path}`); // used to retrieve snapshots
     const writeToFile = (path: string, data: string) => new FSEditor().writeToFile(`${dirToTreeSnapshots}/${path}`, data); // used for saving snapshots
 
-    it('node insertion', () => {
-        const filePurger = new FilePurger(new MockFSEditor());
+    describe('Insertion', () => {
+            
+        it('node insertion', () => {
+            const filePurger = new FilePurger(new MockFSEditor());
+    
+            filePurger.insertPath('a/b/c');
+            filePurger.insertPath('a/b/c/d/e');
+            filePurger.insertPath('m/n/q');
+            filePurger.insertPath('m/p');
+            filePurger.insertPath('m/p/e/f');
+    
+            // Result tree
+            // *
+            // - a
+            //   - b
+            //     - c*
+            //       - d
+            //         - e*
+            // - m
+            //   - n
+            //     - q*
+            //   - p*
+            //     - e
+            //       - f*
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('initialization_with_paths.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
+    
+        it('initialization with paths', () => {
+            const paths = ['a/b/c',
+                'a/b/c/d/e',
+                'm/n/q',
+                'm/p',
+                'm/p/e/f'];
+            const filePurger = new FilePurger(new MockFSEditor(), paths);
+    
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('initialization_with_paths.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
+    
+        it('insert a bulk of paths', () => {
+            const filePurger = new FilePurger(new MockFSEditor());
+            
+            filePurger.insertPath('a/b/c');
+            filePurger.insertPath('a/b/c/d/e');
+    
+            filePurger.insertPaths(['m/n/q', 'm/p', 'm/p/e/f']);
+    
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('initialization_with_paths.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
+    
+        it('insert a path that is part of another path', () => {
+            const filePurger = new FilePurger(new MockFSEditor());
+    
+            filePurger.insertPath('a/b/c');
+            filePurger.insertPath('a/b/c/d/e');
+            filePurger.insertPath('a/b/k');
+            filePurger.insertPath('d/m/h/z/f');
+            filePurger.insertPath('d/m/h');
+    
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('path_part_of_another.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
+    
+        it('insert single paths', () => {
+            const filePurger = new FilePurger(new MockFSEditor());
+    
+            filePurger.insertPath('d');
+            filePurger.insertPath('a');
+            filePurger.insertPath('e');
+            filePurger.insertPath('f');
+    
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('single_paths.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
+    
+        it('insert duplicate paths', () => {
+            const filePurger = new FilePurger(new MockFSEditor());
+    
+            filePurger.insertPath('d');
+            filePurger.insertPath('a/b/c');
+            filePurger.insertPath('e/f');
+            filePurger.insertPath('a/b/c');
+            filePurger.insertPath('e/f');
+    
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('duplicate_paths.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
+    
+        it('test inserting a path with extra slashes in path', () => {
+            const filePurger = new FilePurger(new MockFSEditor());
+    
+            filePurger.insertPath('/a/b/c/');
+            filePurger.insertPath('/d/e//f///');
+    
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('extra_slashes.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
+    
+        it('tree with branches with only one child', () => {
+            const filePurger = new FilePurger(new MockFSEditor());
+    
+            filePurger.insertPath('/a/b');
+            filePurger.insertPath('/m/n/o/q');
+            filePurger.insertPath('/b/a');
+            filePurger.insertPath('/c/d/e/f');
+    
+            const rootNode = YAML.stringify(filePurger['rootNode']);
+            const expectedTree = readFile('one_child_branches.yml');
+            
+            expect(rootNode).to.equal(expectedTree);
+        });
 
-        filePurger.insertPath('a/b/c');
-        filePurger.insertPath('a/b/c/d/e');
-        filePurger.insertPath('m/n/q');
-        filePurger.insertPath('m/p');
-        filePurger.insertPath('m/p/e/f');
+    });
 
-        // Result tree
-        // *
-        // - a
-        //   - b
-        //     - c*
-        //       - d
-        //         - e*
-        // - m
-        //   - n
-        //     - q*
-        //   - p*
-        //     - e
-        //       - f*
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('initialization_with_paths.yml');
+    describe('Generate purge list', () => {
         
-        expect(rootNode).to.equal(expectedTree);
+        it('creating a purge list from tree', () => {
+            const paths = ['a/b',
+                'a/b/d',
+                'a/b/f/h',
+                'a/b/g/m',
+                'a/c',
+                'a/e/m/o',
+                'a/e/m/p',
+                'a/e/h/q/n',
+                'a/e/h/f/c',
+                'a/e/h/h',
+                'a/e/h'];
+    
+            const filePurger = new FilePurger(new MockFSEditor(), paths);
+            const expectedPurgeList = [ '/a/b', '/a/c', '/a/e/m/o', '/a/e/m/p', '/a/e/h' ];
+    
+            expect(filePurger.purgeList).to.eql(expectedPurgeList);
+        });
+    
+        it('file tree with no common nodes', () => {
+            const paths = ['a/b/c',
+                'm/n/o/p',
+                'q/e/f',
+                'c',
+                'd/m'];
+    
+            const filePurger = new FilePurger(new MockFSEditor(), paths);
+            const expectedPurgeList = ['/a/b/c',
+                '/m/n/o/p',
+                '/q/e/f',
+                '/c',
+                '/d/m'];
+    
+            expect(filePurger.purgeList).to.eql(expectedPurgeList);
+        });
+
     });
 
-    it('initialization with paths', () => {
-        const paths = ['a/b/c',
-            'a/b/c/d/e',
-            'm/n/q',
-            'm/p',
-            'm/p/e/f'];
-        const filePurger = new FilePurger(new MockFSEditor(), paths);
+    describe('Moving purge files', () => {
+    
+        it('moving purging files to a folder (making sure it calls the move method)', () => {
+            const mockFSEditor = new MockFSEditor();
+            const result: [string, string][] = [];
+            mockFSEditor.moveFileToFolder = (from: string, to: string) => {
+                result.push([from, to]);
+            };
 
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('initialization_with_paths.yml');
-        
-        expect(rootNode).to.equal(expectedTree);
-    });
+            mockFSEditor.makeDirectory = (dirName: string) => {
+                expect(dirName).to.equal('/purge');
+            };
 
-    it('insert a bulk of paths', () => {
-        const filePurger = new FilePurger(new MockFSEditor());
-        
-        filePurger.insertPath('a/b/c');
-        filePurger.insertPath('a/b/c/d/e');
+            const filePurger = new FilePurger(mockFSEditor);
 
-        filePurger.insertPaths(['m/n/q', 'm/p', 'm/p/e/f']);
+            filePurger.insertPath('/a/b/c');
+            filePurger.insertPath('/a/d');
+            filePurger.insertPath('/a/b/c/e');
+            filePurger.insertPath('/a/b/c/f');
 
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('initialization_with_paths.yml');
-        
-        expect(rootNode).to.equal(expectedTree);
-    });
-
-    it('insert a path that is part of another path', () => {
-        const filePurger = new FilePurger(new MockFSEditor());
-
-        filePurger.insertPath('a/b/c');
-        filePurger.insertPath('a/b/c/d/e');
-        filePurger.insertPath('a/b/k');
-        filePurger.insertPath('d/m/h/z/f');
-        filePurger.insertPath('d/m/h');
-
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('path_part_of_another.yml');
-        
-        expect(rootNode).to.equal(expectedTree);
-    });
-
-    it('insert single paths', () => {
-        const filePurger = new FilePurger(new MockFSEditor());
-
-        filePurger.insertPath('d');
-        filePurger.insertPath('a');
-        filePurger.insertPath('e');
-        filePurger.insertPath('f');
-
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('single_paths.yml');
-        
-        expect(rootNode).to.equal(expectedTree);
-    });
-
-    it('insert duplicate paths', () => {
-        const filePurger = new FilePurger(new MockFSEditor());
-
-        filePurger.insertPath('d');
-        filePurger.insertPath('a/b/c');
-        filePurger.insertPath('e/f');
-        filePurger.insertPath('a/b/c');
-        filePurger.insertPath('e/f');
-
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('duplicate_paths.yml');
-        
-        expect(rootNode).to.equal(expectedTree);
-    });
-
-    it('test inserting a path with extra slashes in path', () => {
-        const filePurger = new FilePurger(new MockFSEditor());
-
-        filePurger.insertPath('/a/b/c/');
-        filePurger.insertPath('/d/e//f///');
-
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('extra_slashes.yml');
-        
-        expect(rootNode).to.equal(expectedTree);
-    });
-
-    it('tree with branches with only one child', () => {
-        const filePurger = new FilePurger(new MockFSEditor());
-
-        filePurger.insertPath('/a/b');
-        filePurger.insertPath('/m/n/o/q');
-        filePurger.insertPath('/b/a');
-        filePurger.insertPath('/c/d/e/f');
-
-        const rootNode = YAML.stringify(filePurger['rootNode']);
-        const expectedTree = readFile('one_child_branches.yml');
-        
-        expect(rootNode).to.equal(expectedTree);
-    });
-
-    it('creating a purge list from tree', () => {
-        const paths = ['a/b',
-            'a/b/d',
-            'a/b/f/h',
-            'a/b/g/m',
-            'a/c',
-            'a/e/m/o',
-            'a/e/m/p',
-            'a/e/h/q/n',
-            'a/e/h/f/c',
-            'a/e/h/h',
-            'a/e/h'];
-
-        const filePurger = new FilePurger(new MockFSEditor(), paths);
-        const expectedPurgeList = [ '/a/b', '/a/c', '/a/e/m/o', '/a/e/m/p', '/a/e/h' ];
-
-        expect(filePurger.purgeList).to.eql(expectedPurgeList);
-    });
-
-    it('file tree with no common nodes', () => {
-        const paths = ['a/b/c',
-            'm/n/o/p',
-            'q/e/f',
-            'c',
-            'd/m'];
-
-        const filePurger = new FilePurger(new MockFSEditor(), paths);
-        const expectedPurgeList = ['/a/b/c',
-            '/m/n/o/p',
-            '/q/e/f',
-            '/c',
-            '/d/m'];
-
-        expect(filePurger.purgeList).to.eql(expectedPurgeList);
-    });
-
-    it('moving purging files to a folder (making sure it calls the move method)', () => {
-        const mockFSEditor = new MockFSEditor();
-        const result: [string, string][] = [];
-        mockFSEditor.moveFileToFolder = (from: string, to: string) => {
-            result.push([from, to]);
-        };
-
-        mockFSEditor.makeDirectory = (dirName: string) => {
-            expect(dirName).to.equal('/purge');
-        };
-
-        const filePurger = new FilePurger(mockFSEditor);
-
-        filePurger.insertPath('/a/b/c');
-        filePurger.insertPath('/a/d');
-        filePurger.insertPath('/a/b/c/e');
-        filePurger.insertPath('/a/b/c/f');
-
-        filePurger.purge('/purge');
-        expect(result).to.eql([['/a/b/c', '/purge'], ['/a/d', '/purge']]);
+            filePurger.purge('/purge');
+            expect(result).to.eql([['/a/b/c', '/purge'], ['/a/d', '/purge']]);
+        });
+   
     });
 });
--- tests/DirManager/flattenFileTree.test.ts
+++ tests/DirManager/flattenFileTree.test.ts
@@ -4,89 +4,97 @@ import { FlattenFileTree } from '../../src/parser/DirManager/FlattenFileTree';
 import MockDirTreeCreator from '../stub/MockDirTreeCreator';
 import MockFSEditor from '../stub/MockFSEditor';
 
-describe('reorginize series folders [flatten]', function () {
+describe('Flatten File Tree', function () {
     const rootFolder = '/Users/michelbalamou/Downloads/Film_backend/tests/ExampleTrees/example_folders';
 
-    it('well structured series folder', () => {
-        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
-        const flatData = flattenFileTree['findMisplacedFiles']('well_orginized_show');
-        
-        expect(flatData.moveup).to.eql([]);
-        expect(flatData.purge).to.eql([]);
+    describe('Find misplaced files - Purge', () => {
+
+        it('well structured series folder', () => {
+            const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+            const flatData = flattenFileTree['findMisplacedFiles']('well_orginized_show');
+            
+            expect(flatData.moveup).to.eql([]);
+            expect(flatData.purge).to.eql([]);
+        });
+
+        it('extra non-video files in series top level', () => {
+            const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+            const flatData = flattenFileTree['findMisplacedFiles']('extra_files_in_top_level');
+
+            const purge = [ `${rootFolder}/extra_files_in_top_level/movie_description.txt`,
+                            `${rootFolder}/extra_files_in_top_level/poster.jpeg` ];
+
+            expect(flatData.moveup).to.eql([]);
+            expect(flatData.purge).to.eql(purge);
+        });
+
+        it('empty folders in series top level', () => {
+            const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+            const flatData = flattenFileTree['findMisplacedFiles']('empty_folders_in_top_level');
+
+            const purge =[ `${rootFolder}/empty_folders_in_top_level/S3`,
+                            `${rootFolder}/empty_folders_in_top_level/information` ];
+
+            expect(flatData.moveup).to.eql([]);
+            expect(flatData.purge).to.eql(purge);
+        });
+
+        it('folders with no video files in series top level', () => {
+            const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+            const flatData = flattenFileTree['findMisplacedFiles']('folders_with_no_video_files_in_top_level');
+            
+            const purge =[ `${rootFolder}/folders_with_no_video_files_in_top_level/S3/infromation.txt`,
+                            `${rootFolder}/folders_with_no_video_files_in_top_level/S3/some_picture.jpeg`,
+                            `${rootFolder}/folders_with_no_video_files_in_top_level/S3` ];
+
+            expect(flatData.moveup).to.eql([]);
+            expect(flatData.purge).to.eql(purge);
+        });
+
+        it('folders and non-video files at depth 2', () => {
+            const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+            const flatData = flattenFileTree['findMisplacedFiles']('folders_and_non_video_files_at_depth_2');
+            
+            const purge = [`${rootFolder}/folders_and_non_video_files_at_depth_2/S1/depth_1_folder`,
+                            `${rootFolder}/folders_and_non_video_files_at_depth_2/S2/depth_1_folder`];
+
+            expect(flatData.moveup).to.eql([]);
+            expect(flatData.purge).to.eql(purge);
+        });
+
     });
 
-    it('extra non-video files in series top level', () => {
-        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
-        const flatData = flattenFileTree['findMisplacedFiles']('extra_files_in_top_level');
+    describe('Find misplaced files - Move Up', () => {
 
-        const purge = [ `${rootFolder}/extra_files_in_top_level/movie_description.txt`,
-                        `${rootFolder}/extra_files_in_top_level/poster.jpeg` ];
+        it('video files at level 3', () => {
+            const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+            const flatData = flattenFileTree['findMisplacedFiles']('video_files_at_level_3');
+            
+            const moveUp = [{path: `${rootFolder}/video_files_at_level_3/S2/nested_videos/South park [3x02].mkv`, level: 3},
+                            {path: `${rootFolder}/video_files_at_level_3/S2/nested_videos/South park [3x03].mp4`, level: 3}];
 
-        expect(flatData.moveup).to.eql([]);
-        expect(flatData.purge).to.eql(purge);
-    });
+            const purge = [`${rootFolder}/video_files_at_level_3/S2/nested_videos`];
 
-    it('empty folders in series top level', () => {
-        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
-        const flatData = flattenFileTree['findMisplacedFiles']('empty_folders_in_top_level');
+            expect(flatData.moveup).to.eql(moveUp);
+            expect(flatData.purge).to.eql(purge);
+        });
 
-        const purge =[ `${rootFolder}/empty_folders_in_top_level/S3`,
-                        `${rootFolder}/empty_folders_in_top_level/information` ];
+        it('video files below level 2', () => {
+            const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
+            const flatData = flattenFileTree['findMisplacedFiles']('video_files_below_level_2');
+            
+            const moveUp = [{ path: `${rootFolder}/video_files_below_level_2/Season 12/nested_videos/South park [3x02].mkv`, level: 3},
+                            { path: `${rootFolder}/video_files_below_level_2/Season 10/nested_videos/deeply nested/South park [3x02].mkv`, level: 4},
+                            { path: `${rootFolder}/video_files_below_level_2/Season 10/nested_videos/deeply nested/South park [3x03].mp4`, level: 4},
+                            { path: `${rootFolder}/video_files_below_level_2/Season 12/nested_videos/deeply nested/South park [3x03].mp4`, level: 4}];
 
-        expect(flatData.moveup).to.eql([]);
-        expect(flatData.purge).to.eql(purge);
-    });
+            const purge = [`${rootFolder}/video_files_below_level_2/Season 10/nested_videos`,
+                            `${rootFolder}/video_files_below_level_2/Season 12/nested_videos`];
 
-    it('folders with no video files in series top level', () => {
-        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
-        const flatData = flattenFileTree['findMisplacedFiles']('folders_with_no_video_files_in_top_level');
-        
-        const purge =[ `${rootFolder}/folders_with_no_video_files_in_top_level/S3/infromation.txt`,
-                        `${rootFolder}/folders_with_no_video_files_in_top_level/S3/some_picture.jpeg`,
-                        `${rootFolder}/folders_with_no_video_files_in_top_level/S3` ];
+            expect(flatData.moveup, 'move up wrong').to.eql(moveUp);
+            expect(flatData.purge, 'purge wrong').to.eql(purge);
+        });
 
-        expect(flatData.moveup).to.eql([]);
-        expect(flatData.purge).to.eql(purge);
-    });
-
-    it('folders and non-video files at depth 2', () => {
-        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
-        const flatData = flattenFileTree['findMisplacedFiles']('folders_and_non_video_files_at_depth_2');
-        
-        const purge = [`${rootFolder}/folders_and_non_video_files_at_depth_2/S1/depth_1_folder`,
-                        `${rootFolder}/folders_and_non_video_files_at_depth_2/S2/depth_1_folder`];
-
-        expect(flatData.moveup).to.eql([]);
-        expect(flatData.purge).to.eql(purge);
-    });
-
-    it('video files at level 3', () => {
-        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
-        const flatData = flattenFileTree['findMisplacedFiles']('video_files_at_level_3');
-        
-        const moveUp = [{path: `${rootFolder}/video_files_at_level_3/S2/nested_videos/South park [3x02].mkv`, level: 3},
-                        {path: `${rootFolder}/video_files_at_level_3/S2/nested_videos/South park [3x03].mp4`, level: 3}];
-
-        const purge = [`${rootFolder}/video_files_at_level_3/S2/nested_videos`];
-
-        expect(flatData.moveup).to.eql(moveUp);
-        expect(flatData.purge).to.eql(purge);
-    });
-
-    it('video files below level 2', () => {
-        const flattenFileTree = new FlattenFileTree(new MockDirTreeCreator(), new MockFSEditor());
-        const flatData = flattenFileTree['findMisplacedFiles']('video_files_below_level_2');
-        
-        const moveUp = [{ path: `${rootFolder}/video_files_below_level_2/Season 12/nested_videos/South park [3x02].mkv`, level: 3},
-                        { path: `${rootFolder}/video_files_below_level_2/Season 10/nested_videos/deeply nested/South park [3x02].mkv`, level: 4},
-                        { path: `${rootFolder}/video_files_below_level_2/Season 10/nested_videos/deeply nested/South park [3x03].mp4`, level: 4},
-                        { path: `${rootFolder}/video_files_below_level_2/Season 12/nested_videos/deeply nested/South park [3x03].mp4`, level: 4}];
-
-        const purge = [`${rootFolder}/video_files_below_level_2/Season 10/nested_videos`,
-                        `${rootFolder}/video_files_below_level_2/Season 12/nested_videos`];
-
-        expect(flatData.moveup, 'move up wrong').to.eql(moveUp);
-        expect(flatData.purge, 'purge wrong').to.eql(purge);
     });
 
     describe('Move Up', () => {
@@ -129,6 +137,6 @@ describe('reorginize series folders [flatten]', function () {
     
             expect(result).to.eql(expectation);
         });
-        
+
     });
 });
